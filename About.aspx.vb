3558bd96adfb416a25a67942f2234fe5b09cde7d
chris <chris@PINTO>
2025-09-23 19:54:47 -0600

    Agregar archivos de proyecto.

---
+++ About.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class About
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        Dim p1 As New Persona()
+
+    End Sub
+End Class
